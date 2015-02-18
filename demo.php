d74ce63176ee7d2e362dc82de83da4d04e4ac7b8
webforce3 <denis.nerborac@wf3.fr>
2015-02-18 12:18:09 +0100

    Example script with usage of image search function

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php include_once 'inc/google_img.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Google Image API Demo</title>	
+</head>
+<body>
+	<img src="<?= getGoogleImg('php logo') ?>" />
+</body>
+</html>
