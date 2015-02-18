1b06bb0e572110a996de1548d9b28b295d7c8abf
webforce3 <denis.nerborac@wf3.fr>
2015-02-18 12:21:56 +0100

    Add a search parameter from URL and call the function with it

--- demo.php
+++ demo.php
@@ -1,4 +1,12 @@
-<?php include_once 'inc/google_img.php'; ?>
+<?php 
+include_once 'inc/google_img.php';
+
+$search = !empty($_GET['search']) ? $_GET['search'] : '';
+
+if (empty($search)) {
+	exit('Empty search parameter in URL.<br />Call page with ?search=... to use it.');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +14,6 @@
 	<title>Google Image API Demo</title>	
 </head>
 <body>
-	<img src="<?= getGoogleImg('php logo') ?>" />
+	<img src="<?= getGoogleImg($search) ?>" />
 </body>
 </html>
